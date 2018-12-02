2ff837c307f39486a55d4b5164ff5d8da616a6e7
Glutek <glutonator@gmail.com>
2018-12-02 21:49:46 +0100

    testone.py histogram is back

--- testone.py
+++ testone.py
@@ -13,28 +13,28 @@ def localBinaryPatterns(image, numPoints, radius, eps=1e-7):
                                        radius, method="uniform")
     # print(lbp)
 
-
     # cv2.imshow("image", image)
     # cv2.imwrite('color_img.jpg', lbp)
     # cv2.waitKey();
 
-    # (hist, _) = np.histogram(lbp.ravel(),
-    #                          bins=np.arange(0, numPoints + 3),
-    #                          range=(0, numPoints + 2))
-    #
-    # # normalize the histogram
-    # hist = hist.astype("float")
-    # hist /= (hist.sum() + eps)
+    (hist, _) = np.histogram(lbp.ravel(),
+                             bins=np.arange(0, numPoints + 3),
+                             range=(0, numPoints + 2))
+
+    # normalize the histogram
+    hist = hist.astype("float")
+    hist /= (hist.sum() + eps)
 
     # print histogram
-    # plt.hist(lbp.ravel(),
-    #          bins=np.arange(0, self.numPoints + 3),
-    #          range=(0, self.numPoints + 2))
-    # plt.show()
+    plt.hist(lbp.ravel(),
+             bins=np.arange(0, numPoints + 3),
+             range=(0, numPoints + 2))
+    plt.show()
 
     # return the histogram of Local Binary Patterns
-    # return hist
-    return lbp
+    return hist
+    # return lbp
+
 
 def createLBPimage(imagePath, image):
     pointsNumber = 8
@@ -43,25 +43,27 @@ def createLBPimage(imagePath, image):
     image = cv2.imread(imagePath + '/' + image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    lbp = localBinaryPatterns(gray, pointsNumber, radius)
+    hist = localBinaryPatterns(gray, pointsNumber, radius)
 
-    # print(lbp)
+    print(hist)
+    print("#############")
 
-    return lbp
+    return hist
 
 
-def createTraingSet():
+def createTraingSet(path):
     # List with histograms
     x = []
     # List with labels
     y = []
 
     # paths
-    path = './fruits/fruits-360/Test'
+    # path = './fruits/fruits-360/Test'
     dirpath = os.getcwd()
     subfolders = os.listdir(path)
     print(subfolders)
 
+    count = 0
     # get data
     for folder in subfolders:
         print(folder)
@@ -77,19 +79,31 @@ def createTraingSet():
 
             # save label
             y.append(folder)
-
             # break
-        # break
+            if (count == 3):
+                break
+            count += 1
+
+        break
     return x, y
 
 
 # def main():
 print("Hello World!")
-# dataX, dataY = createTraingSet()
+# trainging
+# dataX, dataY = createTraingSet('./fruits/fruits-360/Training')
 # np.save("dataX.npy", dataX)
 # np.save("dataY.npy", dataY)
-dataXXX = np.load("dataX.npy")
-dataYYY = np.load("dataY.npy")
+# dataXXX = np.load("dataX.npy")
+# dataYYY = np.load("dataY.npy")
+
+# testing
+dataX, dataY = createTraingSet('./fruits/fruits-360/Test')
+np.save("dataX-test.npy", dataX)
+np.save("dataY-test.npy", dataY)
+# dataXXX-test = np.load("dataX-test.npy")
+# dataYYY-test = np.load("dataY-test.npy")
+
 
 # if __name__ == "__main__":
 #     main()
